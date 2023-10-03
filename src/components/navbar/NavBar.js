281561e3f2a46a88ece53cb38d9114eeaf3c2531
mathiacost <mathias.acostan@gmail.com>
2023-10-03 13:10:35 -0300

    first commit

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Matir</h3>
+            <div>
+                <button>Minutas</button>
+                <button>Sanguches</button>
+                <button>Olla</button>
+                <button>Bebida</button>
+                <button>Postre</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
